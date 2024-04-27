7dc2dab474b3e0ee307cd148781aa2546691c2c2
Aman14052 <168223551+Aman14052@users.noreply.github.com>
2024-04-27 05:21:32 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//DATA TYPES
+// number => 2 to power 53
+// bigint => big numbers
+// string => ""
+// boolean => true/false
+// null => standalone value => Empty
+// undefined => not empty just value is not given
+// symbol => unique
+
+
+// object
+
+//typeof - shows what is the type of data types
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
